7b9cc6366e6be32f9d6a86ad38bfadf1cb2e9426
Shouvik Banik <shouvikbanik101@gmail.com>
2026-01-30 21:17:16 +0530

    Add Mapping of Answers to Answer Buttons.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:quiz_app/answer_button.dart';
 
+import 'package:quiz_app/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -12,6 +14,8 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
     return SizedBox(
@@ -19,15 +23,14 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question is...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.question,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(answerText: 'Answer 1', onTap: () {}),
-          AnswerButton(answerText: 'Answer 2', onTap: () {}),
-          AnswerButton(answerText: 'Answer 3', onTap: () {}),
-          AnswerButton(answerText: 'Answer 4', onTap: () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );
