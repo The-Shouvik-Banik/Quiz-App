3b4d63a2b0b52548d30096deb7f801e7343e436c
Shouvik Banik <shouvikbanik101@gmail.com>
2026-02-01 00:18:14 +0530

    Add Question Summary Folder to store Question Identifier, Question Summary and Summary Item and all of them help in displaying and styling the Question Summary.

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/questions_summary.dart';
+import 'package:quiz_app/questions_summary/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
